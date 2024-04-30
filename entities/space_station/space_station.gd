8809767ea358b8ad1bb431038cffe66d05a887e1
Joe Zlonicky <joe.zlonicky@gmail.com>
2024-04-30 10:52:57 -0700

    Remove robot side of game

--- entities/space_station/space_station.gd
+++
@@ -1,6 +0,0 @@
-extends StaticBody2D
-
-
-func _on_interactable_area_interacted_with() -> void:
-	var scene_to_change_to: PackedScene = load("res://levels/space_station/space_station.tscn")
-	get_tree().change_scene_to_packed(scene_to_change_to)
