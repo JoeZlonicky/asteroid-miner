feb70204c1c6d80c600c98f1cd4a9b04d9ebbcf6
JoeZlonicky <joe.zlonicky@gmail.com>
2024-05-06 20:18:27 -0700

    Dialogue options

--- components/ui/dialogue/panel/dialogue_panel.gd
+++ components/ui/dialogue/panel/dialogue_panel.gd
@@ -4,11 +4,15 @@ extends PanelContainer
 
 signal started
 signal next_triggered
+signal option_selected(i: int)
 signal finished
+
+const OPTION_SCENE: PackedScene = preload("res://components/ui/dialogue/option/dialogue_option.tscn")
 
 @onready var text_label: Label = %TextLabel
 @onready var name_container: PanelContainer = %NameContainer
 @onready var name_label: Label = %NameLabel
+@onready var option_container: VBoxContainer = %Options
 @onready var visible_characters_tick: Timer = $VisibleCharactersTick
 
 
@@ -29,10 +33,28 @@ func start(character_name: String = "") -> void:
 
 
 func display_text(text: String) -> void:
+	_clear_options()
 	text_label.text = text
 	text_label.visible_characters = 0
 	visible_characters_tick.start()
 	await next_triggered
+
+
+func display_options(text: String, options: Array[String]) -> int:
+	assert(options.size() > 0)
+	_clear_options()
+	text_label.text = text
+	text_label.visible_characters = 0
+	for option_i in options.size():
+		var option_button: Button = OPTION_SCENE.instantiate()
+		option_container.add_child(option_button)
+		option_button.modulate = Color.TRANSPARENT
+		option_button.text = options[option_i]
+		option_button.pressed.connect(_on_option_button_selected.bind(option_i))
+		
+	visible_characters_tick.start()
+	var i: int = await option_selected
+	return i
 
 
 func finish() -> void:
@@ -42,10 +64,27 @@ func finish() -> void:
 
 func _next() -> void:
 	if text_label.visible_characters < text_label.text.length():
-		text_label.visible_characters = text_label.text.length()
+		_fast_forward()
+		return
+	if option_container.get_child_count() > 0:
+		_reveal_options()
 		return
 	
 	next_triggered.emit()
+
+
+func _fast_forward() -> void:
+	text_label.visible_characters = text_label.text.length()
+
+
+func _reveal_options() -> void:
+	for option in option_container.get_children():
+		option.modulate = Color.WHITE
+
+
+func _clear_options() -> void:
+	for option in option_container.get_children():
+		option.queue_free()
 
 
 func _on_visible_characters_tick_timeout() -> void:
@@ -55,3 +94,8 @@ func _on_visible_characters_tick_timeout() -> void:
 	text_label.visible_characters += 1
 	if text_label.visible_characters >= text_label.text.length():
 		visible_characters_tick.stop()
+		_reveal_options()
+
+
+func _on_option_button_selected(i: int) -> void:
+	option_selected.emit(i)
--- content/dialogue/orin/orin.gd
+++ content/dialogue/orin/orin.gd
@@ -7,4 +7,13 @@ func logic(_player: Player, panel: DialoguePanel) -> void:
 	await panel.display_text("It's quite peaceful out here, isn't it?")
 	await panel.display_text("I never get lonely, though.")
 	await panel.display_text("Plenty of stars to keep me company.")
+	
+	var i: int = await panel.display_options("What can I do for you?",
+		["Sell ore", "I'm good, thanks!"])
+	
+	if i == 0:
+		await panel.display_text("I'll put it to good use!")
+	elif i == 1:
+		await panel.display_text("Thanks for stopping by!")
+	
 	panel.finish()
